966ec200fa5ea9755988ef54fca4a09716d774fd
ather ali <a.ngaming102734@gmail.com>
2025-12-10 17:53:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// var arr1=['ather'];
+// document.write(arr1);
+// console.log(arr1);
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+// alert("Welcome to " + city3);
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// VB
+
+// var friute = ['apple', 'bananna',];
+// console.log(friute);
+
+// friute.pop();
+// console.log(friute)
+// friute.push(origin, banan)
+// console.log(friute)
+
+// var friute = ['apple', 'bananna',];
+// console.log (friute);
+
+// friute.shift();
+// console.log(friute);
+
+// friute.unshift(gvhb,);
+
+var friute=['ori','apple','hbu',];
+
+friute.splice(1,1,ligd,mango);
+
+console.log(friute);
